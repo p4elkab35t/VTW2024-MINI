755f8ff8555b4fd3801fc612c88501550424d6cb
Daksh Kashyap <65839160+dakshkashyap@users.noreply.github.com>
2024-09-14 15:53:05 -0700

    fixed button color and updated text color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
    <div className="flex flex-row items-center justify-between mb-5 w-full">
     <h1 className="text-center font-bold text-2xl"><Link href="/">VIBE</Link></h1>
-    <button className="gap-16 mx-3 bg-purple-500 py-[5px] px-[15px] rounded">Login</button>
+    <button className="text-white gap-16 mx-3 bg-violet-700 py-[5px] px-[15px] rounded">Login</button>
   </div>
   )
 }
