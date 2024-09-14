fd840cd6b1a57ca0b17ea6f16a3f1d5f1951b033
p4elkab35t <saganiter234@gmail.com>
2024-09-14 14:27:29 -0700

    event card component created

---
+++ src/components/event.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import { 
+    CardContent,
+    CardDescription,
+    CardHeader, 
+    Card 
+} from "@/components/ui/card";
+
+import { 
+    Avatar,
+    AvatarImage
+ } from "@/components/ui/avatar";
+
+export const EventCard = ({ event }) => {
+    console.log(event);
+
+    const dateForamtted = new Date(event.date).toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+  return (
+    <Card className="w-full h-full max-w-[20rem] max-h-[32rem] p-2 flex flex-col content-around justify-around">
+        <CardContent className="relative aspect-square overflow-hidden p-0">
+            <Image layout="fill" className="rounded-lg" objectPosition="center top" objectFit="cover" src={event.logo.src} alt={event.logo.alt} />
+        </CardContent>
+        <CardHeader className="text-left p-0 pt-4 font-[500]">
+            <p className="event-card__date text-[#6200EE] text-base font-medium">{dateForamtted}</p>
+            <h2 className="event-card__title font-bold text-lg">{event.title}</h2>
+            <p className="event-card__description text-base text-[#7E7E7E]">{event.description}</p>
+            <div className="pt-2 flex flex-row  -space-x-4">
+                {event.attendees.map((attendee, index) => {
+                    return (
+                        <Avatar key={index}  alt={attendee.avatar.alt} className="border-4 border-white">
+                            <AvatarImage src={attendee.avatar.src} />    
+                        </Avatar>
+                    );
+                })}
+            </div>
+        </CardHeader>
+
+        {/* <div className="attendees">
+           
+            <div className="attendees__avatars flex space-x-2">
+                <div className="attendees__avatar">
+                    <Image className="rounded-full" src="/images/sample_avatar.jpg" alt="Sample Avatar" width={24} height={24} />
+                </div>
+                <div className="attendees__avatar">
+                    <Image className="rounded-full" src="/images/sample_avatar.jpg" alt="Sample Avatar" width={24} height={24} />
+                </div>
+                <div className="attendees__avatar">
+                    <Image className="rounded-full" src="/images/sample_avatar.jpg" alt="Sample Avatar" width={24} height={24} />
+                </div>
+            </div>
+        </div> */}
+    </Card>
+  );
+}
